647a7c582bea302f68170932d58c43cccd2b5a67
Will Riches <will@riches.im>
2022-06-29 04:06:55 +0100

    Fix broken links

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/guides/intro"
+            to="/guides"
           >
             Let's get started!
           </Link>
