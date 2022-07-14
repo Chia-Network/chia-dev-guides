7a9cc536c9fac14d590447935df5a32df3026174
Christopher Phan <phan.christopher@yahoo.com>
2022-07-13 20:59:39 -0700

    feat: add search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,6 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "Chia-Network", // Usually your GitHub org/user name.
   projectName: "chia-dev-guides", // Usually your repo name.
+  plugins: [require.resolve("@cmfcmf/docusaurus-search-local")],
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
